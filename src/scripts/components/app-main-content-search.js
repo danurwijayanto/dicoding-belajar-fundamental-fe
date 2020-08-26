92d81cdc4831b964355977f6a5ce661a2f548886
danurwijayanto <danurwijayanto@gmail.com>
2020-08-26 15:23:31 +0700

    change footer

---
+++ src/scripts/components/app-main-content-search.js
@@ -0,0 +1,42 @@
+class AppMainContentSearch extends HTMLElement {
+    set movies(movies) {
+        this._movies = movies;
+        this.render();
+    }
+
+    render() {
+        console.log(this._movies);
+        let div = '';
+
+        let i = 0;
+        let j = 2;
+        this._movies.forEach(movie => {
+            const fullImageUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+            const movieDescription = (movie.overview).substring(0, 250) + ".... Read more";
+
+            if (i % 3 === 0 || i == 0) div += "<div class='row'>";
+            div += `
+            <div class="col s12 m4 hoverable" id="${movie.id}">
+                <div class="icon-block">
+                    <div style="padding: 25px">
+                        <img class="responsive-img" src="${fullImageUrl}" alt="Fan Art">  
+                        <h5 class="center">${movie.name}</h5>
+                        <p class="light">${movieDescription}</p>
+                    </div>
+                </div>
+            </div>
+            `;
+
+            if (j == i || i === 2 || ((this._movies).length - 1) === i) {
+                j += 3
+                div += "</div>";
+
+            }
+
+            this.innerHTML = div;
+            i++;
+        });
+    }
+}
+
+customElements.define("app-main-content-search", AppMainContentSearch);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 import "./components/app-main-content-list.js"
+import "./components/app-main-content-search.js"
 
 function main() {
     // Element
@@ -9,7 +10,49 @@ function main() {
     const imageUrl = "https://image.tmdb.org/t/p/w500"
 
     const searchMovieByName = (name) => {
-        alert('fungsi');
+        if (name == 0) {
+            getPopularMovie();
+            return;
+        }
+
+        const xhr = new XMLHttpRequest();
+        //menetapkan callback jika response sukses dan error
+        xhr.onload = function() {
+            const responseJson = JSON.parse(this.responseText);
+            if (responseJson.error) {
+                showResponseMessage(responseJson.message);
+            } else {
+                homePageRenderSearch(responseJson.results);
+            }
+        }
+
+        xhr.onerror = function() {
+            showResponseMessage();
+        }
+
+        xhr.open("GET", `${baseUrl}search/tv${apiKey}&query=${name}`);
+
+        xhr.send();
+    }
+
+    const homePageRenderSearch = (movies) => {
+        const mainContent = document.getElementById("content");
+        const mainContentSearchElement = document.createElement("app-main-content-search");
+        mainContentSearchElement.movies = movies;
+
+        if (mainContent.querySelector("app-main-content-search") == null) {
+            mainContent.replaceChild(mainContentSearchElement, document.querySelector("app-main-content-list"));
+        } else {
+            mainContent.replaceChild(mainContentSearchElement, document.querySelector("app-main-content-search"));
+        }
+
+
+        const buttons = document.querySelectorAll(".hoverable");
+        buttons.forEach(button => {
+            button.addEventListener("click", event => {
+                alert('hellow 2');
+            })
+        })
     }
 
     const getPopularMovie = () => {
@@ -36,27 +79,20 @@ function main() {
     };
 
     const homePageRender = (movies) => {
+        const mainContent = document.getElementById("content");
+
         const mainContentListElement = document.createElement("app-main-content-list");
         mainContentListElement.movies = movies;
 
-        document.getElementById("content").appendChild(mainContentListElement);
-        // const listMovieElement = document.querySelector("#movie-list");
-        // listMovieElement.innerHTML = "";
+        if (mainContent.querySelector("app-main-content-list") == null) {
+            mainContent.appendChild(mainContentListElement);
+        } else if (mainContent.querySelector("app-main-content-search") == null) {
+            mainContent.replaceChild(mainContentListElement, document.querySelector("app-main-content-list"));
+        } else {
+            mainContent.replaceChild(mainContentListElement, document.querySelector("app-main-content-search"));
+        }
 
-        // movies.forEach(movie => {
-        //     const fullImageUrl = imageUrl + movie.poster_path;
-        //     const movieDescription = (movie.overview).substring(0, 250) + ".... Read more";
-        //     listMovieElement.innerHTML += `
-        //     <div class="col s4 m4">
-        //     <div class="icon-block">
-        //     <img class="responsive-img" src="${fullImageUrl}" alt="Fan Art">  
-        //                 <h5 class="center">${movie.name}</h5>
-
-        //                 <p class="light">${movieDescription}</p>
-        //             </div>
-        //         </div>
-        //     `;
-        // });
+        // document.getElementById("content").appendChild(mainContentListElement);
 
         const buttons = document.querySelectorAll(".hoverable");
         buttons.forEach(button => {
@@ -98,13 +134,13 @@ function main() {
 
         //     updateBook(book)
         // });
-        getPopularMovie();
-    });
+        inputSearchElement.addEventListener("keypress", function(e) {
+            if (e.key === 'Enter' || e.keyCode === 13) {
+                searchMovieByName(inputSearchElement.value);
+            }
+        });
 
-    inputSearchElement.addEventListener("keypress", function(e) {
-        if (e.key === 'Enter' || e.keyCode === 13) {
-            searchMovieByName(inputSearchElement.value);
-        }
+        getPopularMovie();
     });
 }
 
